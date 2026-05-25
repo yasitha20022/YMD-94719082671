e0a6daf7537bfc9e93375b6b1b971cb3fa3d8a84
yasitha20022 <146047425+yasitha20022@users.noreply.github.com>
2026-05-25 21:17:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?E8lCy9',
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?PxQ7rf',
     PORT: process.env.PORT || 7860,
     SESSION_NAME: process.env.SESSION_NAME || "auth_info_baileys"
 };
